1db08f509a22bdcebe5f7664c52893a21c613693
shanecodev <shanecodev@gmail.com>
2019-10-30 00:57:11 -0400

    Updates

---
+++ precache-manifest.e7645369ea1e72312d382fc855c960b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d19ca7080d203cfa18ac540b70845ed",
+    "url": "/fitness-app/index.html"
+  },
+  {
+    "revision": "724fb9b67cef12c9676f",
+    "url": "/fitness-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "ac98077048897eacf933",
+    "url": "/fitness-app/static/js/2.f5fdcb00.chunk.js"
+  },
+  {
+    "revision": "724fb9b67cef12c9676f",
+    "url": "/fitness-app/static/js/main.463141c2.chunk.js"
+  },
+  {
+    "revision": "37c205e3b3dee271195d",
+    "url": "/fitness-app/static/js/runtime-main.fdf4dc12.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/fitness-app/static/media/logo.25bf045c.svg"
+  }
+]);
